0a03c73f39c25de6b7f4c7e83ada38a089e0bd8f
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2020-12-23 19:31:49 +0600

    Добавьте файлы проекта.

---
+++ SimpleHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Very Good!");
+        }
+    }
+}
